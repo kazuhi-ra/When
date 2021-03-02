cc5451da1c7393f274ba94697b2eb5240b79f5dd
kazuhi-ra <36134103+kazuhi-ra@users.noreply.github.com>
2021-03-03 01:20:36 +0900

    Create When.tsx

---
+++ When.tsx
@@ -0,0 +1,42 @@
+import { FC, ReactNode } from 'react'
+
+type Props =
+  | {
+      children: ReactNode
+      and?: undefined
+      or?: undefined
+      state: boolean
+      state2?: undefined
+    }
+  | {
+      children: ReactNode
+      and: boolean
+      or?: undefined
+      state: boolean
+      state2: boolean
+    }
+  | {
+      children: ReactNode
+      and?: undefined
+      or: boolean
+      state: boolean
+      state2: boolean
+    }
+
+export const When: FC<Props> = ({
+  children,
+  and = undefined,
+  or = undefined,
+  state = false,
+  state2 = undefined,
+}) => {
+  if (and) {
+    if (!state || !state2) return null
+  } else if (or) {
+    if (!state && !state2) return null
+  } else {
+    if (!state) return null
+  }
+
+  return <>{children}</>
+}
